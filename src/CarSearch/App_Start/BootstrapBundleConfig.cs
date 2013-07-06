b57732eff1e59903c7096ba5229084537d9d52a0
Eric Anderson <eric@vector241.com>
2013-07-06 09:30:27 -0500

    Works to launch a tab with the window sticker

--- src/CarSearch/App_Start/BootstrapBundleConfig.cs
+++ src/CarSearch/App_Start/BootstrapBundleConfig.cs
@@ -1,18 +1,25 @@
 using System.Web.Optimization;
+using CarSearch.App_Start;
+using WebActivatorEx;
 
-[assembly: WebActivatorEx.PostApplicationStartMethod(typeof(CarSearch.App_Start.BootstrapBundleConfig), "RegisterBundles")]
+[assembly: PostApplicationStartMethod(typeof (BootstrapBundleConfig), "RegisterBundles")]
 
 namespace CarSearch.App_Start
 {
-	public class BootstrapBundleConfig
-	{
-		public static void RegisterBundles()
-		{
-			// Add @Styles.Render("~/Content/bootstrap") in the <head/> of your _Layout.cshtml view
-			// Add @Scripts.Render("~/bundles/bootstrap") after jQuery in your _Layout.cshtml view
-			// When <compilation debug="true" />, MVC4 will render the full readable version. When set to <compilation debug="false" />, the minified version will be rendered automatically
-			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap*"));
-			BundleTable.Bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.css", "~/Content/bootstrap-responsive.css"));
-		}
-	}
+    public class BootstrapBundleConfig
+    {
+        public static void RegisterBundles()
+        {
+            // Add @Styles.Render("~/Content/bootstrap") in the <head/> of your _Layout.cshtml view
+            // Add @Scripts.Render("~/bundles/bootstrap") after jQuery in your _Layout.cshtml view
+            // When <compilation debug="true" />, MVC4 will render the full readable version. When set to <compilation debug="false" />, the minified version will be rendered automatically
+            BundleTable.Bundles.Add(
+                new ScriptBundle("~/bundles/bootstrap")
+                    .Include("~/Scripts/bootstrap*")
+                    .Include("~/Scripts/jquery-{version}.js")
+                );
+            BundleTable.Bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.css",
+                                                                                   "~/Content/bootstrap-responsive.css"));
+        }
+    }
 }
